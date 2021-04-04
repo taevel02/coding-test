c530e0f38ce256b2c253039d90c13981a4a66f00
taevel02 <taevel02@gmail.com>
2021-04-04 16:37:21 +0900

    review 10250

--- algorithm/acmicpc/Mathematics/10250.py
+++ algorithm/acmicpc/Mathematics/10250.py
@@ -1,25 +1,9 @@
 t = int(input())
-x = 0
-y = 0
-data = [0 for _ in range(t)]
+data = ['' for _ in range(t)]
 
 for i in range(t):
     h, w, n = map(int, input().split())
-
-    if h == 1:
-        x = int(n / h)
-        y = 1
-    elif n % h == 0:
-        x = int(n / h)
-        y = h
-    else:
-        x = int(n / h + 1)
-        y = int(n % h)
-
-    if x < 10:
-        data[i] = f'{y}0{x}'
-    else:
-        data[i] = f'{y}{x}'
+    data[i] = str((n - 1) % h + 1) + str((n - 1) // h + 1).rjust(2, '0')
 
 for i in data:
     print(i)
