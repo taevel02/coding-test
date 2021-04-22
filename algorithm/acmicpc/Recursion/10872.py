43c1cd66de84dab0a53f95a7ab29085cf2c5e7e5
taevel02 <taevel02@gmail.com>
2021-04-22 14:36:29 +0900

    change function name

--- algorithm/acmicpc/Recursion/10872.py
+++ algorithm/acmicpc/Recursion/10872.py
@@ -1,7 +1,7 @@
-def sol(n):
+def factorial(n):
     if n == 1 or n == 0:
         return 1
-    return n * sol(n - 1)
+    return n * factorial(n - 1)
 
 
-print(sol(int(input())))
+print(factorial(int(input())))
