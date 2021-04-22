8b63d9a5cf94d853a112e55cd6c08fbd9ac68dae
taevel02 <taevel02@gmail.com>
2021-04-22 14:43:17 +0900

    solve boj - 10870

--- algorithm/acmicpc/Recursion/10870.py
+++ algorithm/acmicpc/Recursion/10870.py
@@ -1,1 +1,10 @@
-def
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n <= 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(int(input())))
