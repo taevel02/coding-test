374f682cc14202458cfcb6f6ab16a34fdc29524a
taevel02 <taevel02@gmail.com>
2021-05-09 15:38:19 +0900

    solve boj - 15649; include review(!)

---
+++ algorithm/acmicpc/Backtracking/15649.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+visited = [False]*n
+out = []
+
+
+def solve(depth, n, m):
+    if depth == m:
+        print(*out)
+        return
+
+    for i in range(len(visited)):
+        if not visited[i]:
+            visited[i] = True
+            out.append(i + 1)
+            solve(depth + 1, n, m)
+            visited[i] = False
+            out.pop()
+
+
+solve(0, n, m)
