f28819a8eefd8da4f6ac1127efa17e905d4c42dd
taevel02 <taevel02@gmail.com>
2021-06-11 15:22:44 +0900

    solve boj - 2981; include review(!)

---
+++ algorithm/acmicpc/Mathematics/2981.py
@@ -0,0 +1,24 @@
+import math
+
+n = int(input())
+s = []
+a = []
+gcd = 0
+
+for i in range(n):
+    s.append(int(input()))
+    if i == 1:
+        gcd = abs(s[1] - s[0])
+    gcd = math.gcd(abs(s[i] - s[i-1]), gcd)
+
+gcd_a = int(gcd ** 0.5)
+for i in range(2, gcd_a + 1):
+    if gcd % i == 0:
+        a.append(i)
+        a.append(gcd // i)
+
+a.append(gcd)
+a = list(set(a))
+a.sort()
+
+print(*a)
