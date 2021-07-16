e6e212aeafe4b7ef12e72f74c2a345e9a72ae6e5
taevel02 <taevel02@gmail.com>
2021-07-16 12:41:46 +0900

    solve boj - 11286

---
+++ algorithm/acmicpc/Heap/11286.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+heap = []
+for _ in range(int(sys.stdin.readline())):
+    x = int(sys.stdin.readline())
+
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (abs(x), x))
